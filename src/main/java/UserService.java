2707bef8b3dd5dbbbef043242f2974939cbbda00
Abdulbasit <saadudeenabdulbasitopeyemi@gmail.com>
2026-07-27 00:00:44 +0100

    User service created

---
+++ src/main/java/UserService.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class UserService {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your email: ");
+        String email = sc.nextLine();
+        System.out.println(email);
+    }
+}
