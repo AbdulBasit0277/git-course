fff0640b7510a054deb29f4ff46b87b8e65854ea
Abdulbasit <saadudeenabdulbasitopeyemi@gmail.com>
2026-07-27 08:24:26 +0100

    adding admin service

---
+++ src/main/java/AdminService.java
@@ -0,0 +1,11 @@
+public class AdminService {
+    public static void addUser(){
+        // do something
+    }
+    public static void removeUser(){
+        // do something
+    }
+    public static void changeRole(){
+        // do something
+    }
+}
